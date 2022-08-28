d6318d934ce6ab270a6d3ccda30c1ed6d15f3c14
Moamen Mamdouh <moamen.mamdouh1@gmail.com>
2022-08-28 11:57:41 +0200

    Add files via upload

---
+++ java_practice/src/tau/WeddingCake.java
@@ -0,0 +1,13 @@
+package tau;
+
+public class WeddingCake extends Cake {
+    private int tiers;
+
+    public int getTiers() {
+        return tiers;
+    }
+
+    public void setTiers(int tiers) {
+        this.tiers = tiers;
+    }
+}
